43ec8d6b71af473f71afdacfa5155dc239fee9b8
Adriel <adril_trigger@yahoo.com>
2021-11-11 11:32:06 -0300

    kinda functional message board

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date()
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'This time I will build a functional message board!' });
+  res.render('index', {title: 'Mini Messageboard', messages: messages});
+});
+
+/* Get form page */
+router.get('/new', function(req,res,next) {
+  res.render('form', {title: 'Write your message.'});
+});
+
+router.post('/new', function(req,res,next) {
+  let newMessage = {
+    text: req.body.message,
+    user: req.body.user,
+    added: new Date()
+  };
+  messages.push(newMessage);
+  res.redirect('/');
 });
 
 module.exports = router;
